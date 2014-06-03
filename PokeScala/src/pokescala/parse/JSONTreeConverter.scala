8f4138c4002e8fd3240a481ac70bd05dd1373cde
haferflocken <mooilo.ueu@gmail.com>
2014-06-03 11:40:24 -0700

    Parsing utilities.

--- PokeScala/src/pokescala/parse/JSONTreeConverter.scala
+++ PokeScala/src/pokescala/parse/JSONTreeConverter.scala
@@ -2,11 +2,42 @@ package pokescala.parse
 
 import scala.util.parsing.json.JSONObject
 import scala.util.parsing.json.JSONArray
+import scala.collection.mutable
 
 object JSONTreeConverter {
   
-  def objToMap(obj : JSONObject) : Map[String, Any] = null; // TODO
+  def objToMap(obj : JSONObject) : Map[String, Any] = {
+    if (obj == null || obj.obj == null)
+      return null;
+    
+    val buff = new mutable.HashMap[String, Any];
+    
+    for ((key, value) <- obj.obj) {
+      buff(key) = process(value);
+    }
+    
+    return buff.toMap;
+  };
   
-  def arrayToVector(array : JSONArray) : Vector[Any] = null; // TODO
+  def arrayToVector(array : JSONArray) : Vector[Any] = {
+    if (array == null || array.list == null)
+      return null;
+    
+    val buff = new mutable.ArrayBuffer[Any];
+    
+    for (elem <- array.list) {
+      buff += process(elem);
+    }
+    
+    return buff.toVector;
+  };
+  
+  def process(x : Any) : Any = {
+    if (x.isInstanceOf[JSONObject])
+        return objToMap(x.asInstanceOf[JSONObject]);
+    if (x.isInstanceOf[JSONArray])
+      return arrayToVector(x.asInstanceOf[JSONArray]);
+    return x;
+  }
 
 }
